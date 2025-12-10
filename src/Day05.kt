ed66725c996a03b0d82be5262e045c79632876be
Lo_op <shaikhfarhaan312@gmail.com>
2025-12-10 15:31:18 -0500

    Day05 Part1 Complete

---
+++ src/Day05.kt
@@ -0,0 +1,43 @@
+fun main() {
+//    val testInput = readInput("Day05_test")
+//    println(testInput)
+    fun part1(input: List<String>): Int {
+        //parsing
+        val ids = input.slice(input.indexOf("") + 1..input.lastIndex).map {it.toLong()}
+        val rangeStrings = input.slice(0..<input.indexOf(""))
+        val ranges = rangeStrings.map {
+            val parts = it.split('-')
+            parts[0].toLong()..parts[1].toLong()
+        }
+        return validIDs(ids, ranges, 0)
+    }
+    //TEST
+//    val testAnswer1 = part1(testInput)
+//    println(testAnswer1)
+    //FINAL
+    //Read the input from the `src/Day01.txt` file.
+    val input1 = readInput("Day05")
+    val answer1 = part1(input1)
+    println(answer1)
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+    //TEST
+//    val testAnswer2 = part2(testInput)
+//    println(testAnswer2)
+    //FINAL
+//    val input2 = readInput("Day04")
+//    val answer2 = part2(input2)
+//    println(answer2)
+}
+
+private fun validIDs(ids: List<Long>, ranges: List<LongRange>, index: Int): Int {
+    if (ids.isEmpty() || index >= ranges.size) return 0
+    val currentRange = ranges[index]
+
+    val validIds = ids.filter { it in currentRange }
+    val remainingIds = ids.filterNot { it in currentRange }
+
+    return validIds.size + validIDs(remainingIds, ranges, index + 1)
+}
