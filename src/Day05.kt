6da897ebdb72faa6c21011c43174795a21680395
Lo_op <shaikhfarhaan312@gmail.com>
2025-12-10 23:02:21 -0500

    Day05 Part2 Complete

--- src/Day05.kt
+++ src/Day05.kt
@@ -16,20 +16,50 @@ fun main() {
 //    println(testAnswer1)
     //FINAL
     //Read the input from the `src/Day01.txt` file.
-    val input1 = readInput("Day05")
-    val answer1 = part1(input1)
-    println(answer1)
+//    val input1 = readInput("Day05")
+//    val answer1 = part1(input1)
+//    println(answer1)
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        //parsing
+        val rangeStrings = input.slice(0..<input.indexOf(""))
+        val ranges = rangeStrings.map {
+            val parts = it.split('-')
+            parts[0].toLong()..parts[1].toLong()
+        }
+        val sortedRanges = ranges.sortedBy { it.first }
+        val condensedRanges = mutableListOf<LongRange>()
+
+        //merging
+        var currentRange = sortedRanges.first()
+        for (nextRange in sortedRanges.drop(1)) {
+            //if ranges merge/continuous
+            if (currentRange.last + 1 >= nextRange.first) {
+                currentRange = currentRange.first..maxOf(currentRange.last, nextRange.last)
+            } else {
+                condensedRanges += currentRange
+                currentRange = nextRange
+            }
+        }
+        //add in final range
+        condensedRanges += currentRange
+
+        println(condensedRanges)
+
+        var total = 0L
+        for (range in condensedRanges) {
+            total += range.last - range.first + 1
+        }
+
+        return total
     }
     //TEST
 //    val testAnswer2 = part2(testInput)
 //    println(testAnswer2)
     //FINAL
-//    val input2 = readInput("Day04")
-//    val answer2 = part2(input2)
-//    println(answer2)
+    val input2 = readInput("Day05")
+    val answer2 = part2(input2)
+    println(answer2)
 }
 
 private fun validIDs(ids: List<Long>, ranges: List<LongRange>, index: Int): Int {
